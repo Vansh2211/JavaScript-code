2115d05bbb89fe599c39e6515557bf28f1f1f288
Vansh2211 <168256766+Vansh2211@users.noreply.github.com>
2024-08-29 19:30:06 +0000

    datatypes

--- First_folder/test.js
+++ First_folder/test.js
@@ -3,3 +3,16 @@ const name = "Vansh"
 let lastName = "Mehta"
 collegeName = "JGI"
 
+c
+/*
+constant variable cannot change
+always prefer to use let instead of var because 
+of problem of block and functional scope
+*/
+
+console.log(lastName);
+
+// name = "Vann"
+console.table([lastName,name,collegeName])
+
+
